7b1263779fb8adb87c48bd52bb140743a4746aaa
joao pedro <joaoprado2789@gmail.com>
2026-04-04 12:34:18 -0300

    initial config

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+//Import
+const express = require("express");
+const path = require("path");
+const route = require("./routes/routes");
+//config
+const app = express();
+const PORT = 2789;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", route);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
