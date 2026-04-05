3d0106e1b9cf8333a760c26eb4fac3989c1fdee8
joao pedro <joaoprado2789@gmail.com>
2026-04-05 12:51:13 -0300

    css - edit form style and class tag view; js - edit button

--- public/js/taskActions.js
+++ public/js/taskActions.js
@@ -1,11 +1,51 @@
 document.addEventListener("click", async (e) => {
     const btn = e.target.closest("[data-action]");
     if (!btn) return;
+    const task = btn.closest(".task");
     const action = btn.dataset.action;
     const id = btn.dataset.id;
 
     if (action === "edit") {
-        const taskElement = btn.parentElement.parentElement;
+        const taskView = task.querySelector(".task__view");
+        const editView = task.querySelector(".task__edit-view");
+        taskView.classList.add("hidden");
+        editView.classList.remove("hidden");
+
+        const form = editView.querySelector(".edit-view__form");
+        const currentTitle = taskView.querySelector(".task__title").textContent;
+        const currentDescription = taskView.querySelector(".task__description").textContent;
+        form.querySelector(`input[name="title"]`).value = currentTitle.trim();
+        form.querySelector(`input[name="description"]`).value = currentDescription.trim();
+
+        form.onsubmit = async (SubmitEvent) => {
+            SubmitEvent.preventDefault();
+            const formData = new FormData(form);
+            const updatedData = {
+                title: formData.get("title"),
+                description: formData.get("description"),
+            };
+
+            try {
+                const response = await fetch(`/edit/${id}`, {
+                    method: "PATCH",
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify(updatedData),
+                });
+                if (response.ok) {
+                    window.location.reload();
+                } else {
+                    alert("Failed to edit task");
+                }
+            } catch (error) {
+                console.error("Error - Failed to edit task:", error);
+            }
+        };
+        const cancelBtn = form.querySelector(".edit-view__btn--cancel");
+        cancelBtn.onclick = () => {
+            taskView.classList.remove("hidden");
+            editView.classList.add("hidden");
+            form.onsubmit = null;
+        };
     }
     if (action === "delete") {
         const confirmed = confirm("Do you want to delete this task?");
@@ -21,6 +61,7 @@ document.addEventListener("click", async (e) => {
                 }
             } catch (error) {
                 console.error("Failed to delete:", error);
+                alert("An error occurred while trying to delete the task.");
             }
         }
     }
